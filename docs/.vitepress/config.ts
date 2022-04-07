83e431a22c7a3028a8638053423f6897a8768b31
Igor Khomenko <webigorkiev@gmail.com>
2022-04-08 00:10:45 +0300

    remove vitepress build

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 module.exports = {
     title: "vuetouch",
     description: "Enable tap, swipe, drag, touch, hold, mouse down, mouse up events on DOM Element in vue 3",
-    base: "/docs",
+    base: "/vuetouch-docs",
     lang: "en-GB"
 }
