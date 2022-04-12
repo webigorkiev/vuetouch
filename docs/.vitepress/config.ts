060509cca28a930ac81dbac9f6d7dd5cb15523a8
Igor Khomenko <webigorkiev@gmail.com>
2022-04-12 15:35:31 +0300

    version in docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-    title: "vuetouch",
+    title: "@vuemod/vue-touch v0.0.18",
     description: "Enable tap, swipe, drag, touch, hold, mouse down, mouse up events on DOM Element in vue 3",
     base: "/vuetouch-docs",
     lang: "en-GB"
